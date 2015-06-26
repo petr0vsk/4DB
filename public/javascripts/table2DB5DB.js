e74f1414f9e2eb349bce6e74024f8a8293354c3d
petr0vsk <petr0vskjy.aleksander@gmail.com>
2015-06-26 12:42:22 +0400

    В ежемесячной форме переделываем таблицу 4DB с использованием layout. Создать нормальную редактируемую единую таблицу используя span не удалось. Не получается назначить свойства отдельной cell таблице если ее ячейки были слиты

--- public/javascripts/table2DB5DB.js
+++ public/javascripts/table2DB5DB.js
@@ -1,29 +1,138 @@
 
 webix.ui({
 				container:"table2DB5DB",
-				view:"datatable",
+                id:"layout2DB5DB",
+                //width: 960,
+                rows: [
+                    {template:"Форма 4-ДБ", height: 35},
+                    {template:"Сведения об основных событиях, характеризующих состояние безопасности за прошедшую неделю с ... по ... 2015 г.", height: 35},
+                    {template:"Основные показатели", height: 35},
+                    {cols: [
+                        {
+                            template:"2", width: 50
+                        },
+                        {
+                            template:"Информация о событиях по форме 1-ДБ, которая была направлена в Департамент по безопасности и ИТ за отчетный период (неделю), с обязательым уточнением информации и развитием событий::"
+                        }
 
-				columns:[
-					{ id:"npp",	header:"", width:100 },
-					{ id:"context", header:"" ,width:200},
-					{ id:"value",	header:"" , width:200}
-				],
-                spans:true,
-                select:"cell",
-				autoheight:true,
-				autowidth:true,
-                header:false,
-				data:{
-                    data:[
-                        { id:"sub1", npp:"Общий заголовок"},
-                        { id:1, npp:"1.1", context: "Проведено проверок", value:""},
-                       
-                    ],
-                    spans:[//id, column, width, height, value, css
-                            ["sub1", "npp", 3, 1, null]
-                            
-                    ]
-                }
-			});				
+                    ], height: 95},
+                    {minHeight: 70,
+                        view:"datatable",
+                        header:false,
+                        editable:true,
+                        editaction: "dblclick",
+                        resizeColumn:true,
+                        resizeRow:true,
+                        scrollX:false,
+                        scrollY:false,
+                        columns:[
+                            { id:"npp",	    header:"" , width:50},
+                            { id:"value",	header:"" , width:925 ,editor:"text"}
+                        ],
+                            data:[
+                                { id:"1_1", npp:"1.1", value:""},
+                                { id:"1_2", npp:"1.2", value:"" }
+                            ]
+                    },
+                    {cols: [
+                        {
+                            template:"2", width: 50
+                        },
+                        {
+                            template:"Наиболее значимая информация об обстановке в зоне ответственности ДЗ и вокруг его объектов (информация, необходимая для доклада исполнительному вице-президенту-руководителю Департамента по безопасности и ИТ ОАО АФК \"Система\" в соответсвии с п.1.13. формы 1-ДБ):"
+                        }
+
+                    ], height: 95},
+                    {cols: [
+                        {
+                            template:"2.1", width: 50
+                        },
+                        {
+                            template:"В области обеспечения антитеррористической защищеншности персонала и объектов:"
+                        }
+
+                    ], height: 35},
+                    {minHeight: 70,
+                        view:"datatable",
+                        header:false,
+                        editable:true,
+                        editaction: "dblclick",
+                        resizeColumn:true,
+                        resizeRow:true,
+                        scrollX:false,
+                        scrollY:false,
+                        columns:[
+                            { id:"npp",	    header:"" , width:50},
+                            { id:"value",	header:"" , width:925 ,editor:"text"}
+                        ],
+                        data:[
+                            { id:"1_1", npp:"2.1.1", value:""},
+                            { id:"1_2", npp:"2.1.2", value:"" }
+                        ]
+                    },
+                    {cols: [
+                        {
+                            template:"2.1", width: 50
+                        },
+                        {
+                            template:"В области обеспечения экономической безопасности:"
+                        }
+
+                    ], height: 35},
+                    {minHeight: 70,
+                        view:"datatable",
+                        header:false,
+                        editable:true,
+                        editaction: "dblclick",
+                        resizeColumn:true,
+                        resizeRow:true,
+                        scrollX:false,
+                        scrollY:false,
+                        columns:[
+                            { id:"npp",	    header:"" , width:50},
+                            { id:"value",	header:"" , width:925 ,editor:"text"}
+                        ],
+                        data:[
+                            { id:"1_1", npp:"2.2.1", value:""},
+                            { id:"1_2", npp:"2.2.2", value:"" }
+                        ]
+                    },
+                    {cols: [
+                        {
+                            template:"2.3", width: 50
+                        },
+                        {
+                            template:"В области информмационной безопасности:"
+                        }
+
+                    ], height: 35},
+                    {minHeight: 400,
+                        view:"datatable",
+
+                        header:false,
+                        editable:true,
+                        editaction: "dblclick",
+                        resizeColumn:true,
+                        resizeRow:true,
+                        scrollX:false,
+                        scrollY:false,
+                        rowHeight:200,
+                        columns:[
+                            { id:"npp",	    header:"", width:50, height:200},
+                            { id:"context", header:"", width:300, fillspace:true, css:"multiline"},
+                            { id:"value",	header:"" , width:610 ,editor:"text"}
+                        ],
+                        data:[
+                            { id:"1_1", npp:"2.3.1", context:"Сведения об инцидентах информационной безопасности, направленных за текущий период по форме ДБ-1, и мерах, предпринятых по их расследованию и устранению последствий.", value:""},
+                            { id:"1_2", npp:"2.3.2", context:"Сведения о ключевых событиях информационной безопасности, включая проектную деятельность, взаимодействие с регуляторами, пробемы ИБ, предложения по повышению защищенности", value:"" }
+                        ]
+                    }
+
+
+
+
+
+                ]   // rows:
+            });		// webix.ui
 	
 
--- public/javascripts/table4DB.js
+++ public/javascripts/table4DB.js
@@ -4,6 +4,8 @@ table4D = webix.ui({
 				container:"table4DB",
 				view:"datatable",
                 id:"table4D",
+                editable:true,
+                editaction: "dblclick",
                 autoConfig:true,
                 autoheight:true,
                 autowidth:true,
@@ -11,7 +13,8 @@ table4D = webix.ui({
                 resizeRow:true,
                 adjust:true,
                 spans:true,
-                select:"row",
+                select:"cell",
+
 				header:false,
                 columns:[
 					{ id:"npp",	    header:"" , width:50, fillspace:true},
@@ -20,13 +23,12 @@ table4D = webix.ui({
 				],
                 data:{
                     data:[
-                        { id:"header_a", npp:"Форма 4-ДБ", $css:{"text-align":"right"}},
+                        { id:"header_a", npp:"Форма 4-ДБ"},
                         { id:"header_b", npp:"Сведения об основных событиях, характеризующих состояние безопасности за прошедшую неделю с ... по ... 2015 г."},
                         { id:"header_c", npp:"Основные показатели" },
                         { id:"header_1", npp:"1.", context:"Наиболее значимая информация об обстановке в зоне ответственности ДЗ и вокруг его объектов (информация, необходимая для доклада исполнительному вице-президенту-руководителю Департамента по безопасности и ИТ ОАО АФК \"Система\" в соответсвии с п.1.13. формы 1-ДБ):" },
-                        
-                        { id:"1_1", npp:"1.1", context:""}, 
-                        { id:"1_2", npp:"1.2", context:"" }, 
+                        { id:"1_1", npp:"1.1", context:""},
+                        { id:"1_2", npp:"1.2" },
                         { id:"header_2", npp:"2", context:"Информация о событиях по форме 1-ДБ, которая была направлена в Департамент по безопасности и ИТ за отчетный период (неделю), с обязательым уточнением информации и развитием событий:" },
                         { id:"header_2_1", npp:"2.1", context:"В области обеспечения антитеррористической защищеншности персонала и объектов:" },
                         { id:"2_1_1", npp:"2.1.1", context:"" }, 
@@ -35,27 +37,27 @@ table4D = webix.ui({
                         { id:"2_2_1", npp:"2.2.1.", context:"" }, 
                         { id:"2_2_2", npp:"2.2.2.", context:"" },
                         { id:"header_2_3", npp:"2.3.", context:"В области информационной безопасности:" },
-                        { id:"2_3_1", npp:"2.3.1.", context:"Сведения об инцидентах информационной безопасности, направленных за текущий период по форме ДБ-1, и мерах, предпринятых по их расследованию и устранению последствий.", value: "" }, 
-                        { id:"2_3_2", npp:"2.3.2.", context:"Сведения о ключевых событиях информационной безопасности, включая проектную деятельность, взаимодействие с регуляторами, пробемы ИБ, предложения по повышению защищенности", value: "" },
-                         { id:"header_3", npp:"3.", context:"Информация о достигнутых в ходе проверок экономической деятельности наиболее значимых результатах" },
-                        { id:"3_1", npp:"3.1", context:"Проведено проверок финансово-хозяйственной деяетльности (кол-во)", value: "" }, 
-                        { id:"3_2", npp:"3.2.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_3", npp:"3.3.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_4", npp:"3.4.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_5", npp:"3.5.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_6", npp:"3.6.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_7", npp:"3.7.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_8", npp:"3.8.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_9", npp:"3.9.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_10", npp:"3.10.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_11", npp:"3.11.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_12", npp:"3.12.", context:"Установленный ущерб (руб.)", value: "" },
-                        { id:"3_13", npp:"3.13.", context:"Установленный ущерб (руб.)", value: "" },
+                        { id:"2_3_1", npp:"2.3.1.", context:"Сведения об инцидентах информационной безопасности, направленных за текущий период по форме ДБ-1, и мерах, предпринятых по их расследованию и устранению последствий.", value: "",  $cellCss:{value:"editable"} },
+                        { id:"2_3_2", npp:"2.3.2.", context:"Сведения о ключевых событиях информационной безопасности, включая проектную деятельность, взаимодействие с регуляторами, пробемы ИБ, предложения по повышению защищенности", value: "", $cellCss:{value:"editable"} },
+                        { id:"header_3", npp:"3.", context:"Информация о достигнутых в ходе проверок экономической деятельности наиболее значимых результатах" },
+                        { id:"3_1", npp:"3.1", context:"Проведено проверок финансово-хозяйственной деяетльности (кол-во)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_2", npp:"3.2.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_3", npp:"3.3.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_4", npp:"3.4.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_5", npp:"3.5.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_6", npp:"3.6.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_7", npp:"3.7.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_8", npp:"3.8.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_9", npp:"3.9.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_10", npp:"3.10.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_11", npp:"3.11.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_12", npp:"3.12.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
+                        { id:"3_13", npp:"3.13.", context:"Установленный ущерб (руб.)", value: "", $cellCss:{value:"editable"} },
                         { id:"header_4", npp:"4", context:"Информация о выявленных угрозах экономическим интересам Корпоарции, ДЗК и ДЗО, имеющих признаки хищения, а также полученной сигнальной информации о противоправных действиях юридических/физических лиц, наносящих ДЗК и ДЗО материальный ущерб и/или ущерб деловой репутации." },
-                         { id:"4_1", npp:"4.1.", context:"" }, 
-                         { id:"4_2", npp:"4.2.", context:"" }, 
-                         { id:"4_3", npp:"4.3.", context:"" }, 
-                         { id:"4_4", npp:"4.4.", context:"" }
+                         { id:"4_1", npp:"4.1.", value:"", $cellCss:{value:"editable"} },
+                         { id:"4_2", npp:"4.2.", context:"", $cellCss:{value:"editable"} },
+                         { id:"4_3", npp:"4.3.", context:"", $cellCss:{value:"editable"} },
+                         { id:"4_4", npp:"4.4.", context:"", $cellCss:{value:"editable"} }
                         
                         
                       //  { id:1,   npp:"3.1", context: "Проведено проверок", value:""}
@@ -65,7 +67,7 @@ table4D = webix.ui({
                             ["header_a", "npp", 3, 1, null],
                             ["header_b", "npp", 3, 1, null],
                             ["header_c", "npp", 3, 1, null],
-                            ["header_1", "context", 2, 1, null],
+                            ["header_1", "context", 3, 1, null],
                            // ["header_1", "npp", 1, 2, null],
                             ["1_1", "context", 2, 1, null],
                             ["1_2", "context", 2, 1, null],
@@ -99,7 +101,10 @@ table4D = webix.ui({
     table4D.setRowHeight("header_2", 100);
     table4D.setRowHeight("2_3_1", 200);
     table4D.setRowHeight("2_3_2", 200);
-     
+    table4D.setRowHeight("3_1", 100);
+    table4D.setRowHeight("header_4", 100);
+    $$("table4D").addCellCss("1_1", "context", "editable");
+    table4D.editCell("2_3_1", "value", true, true);
     
     });	// ---- webix.ready
 	
