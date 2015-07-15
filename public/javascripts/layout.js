ed883dd5ffb7e3dc7ac643074ab150f4941752e9
petr0vsk <petr0vskjy.aleksander@gmail.com>
2015-07-15 17:42:42 +0400

    Создаем таблицу в виде формы с редактируемыми элементами

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -2,7 +2,7 @@
  * Created by avpetro1 on 14.07.2015.
  */
 
-
+//----------------------------------------
 var toolbar = {
     view:"toolbar", paddingY:0, elements:[
         {view:"tabbar", id:'tabbar', value: 'MainView', multiview:true, options: [
@@ -12,39 +12,71 @@ var toolbar = {
         }
     ]
 };
-
+//---------------------------------------
 var  MainPage = {
     id:"MainView",
     container: "MainContainer",
     view: "template",
     template: "Главная страница"
 };
-
+//-----------------------------------------
 var  tab4_DB = {
     id:"4DBView",
     container: "MainContainer",
     type:"line",
+    editable:true,
+    editor:"text",
     rows: [
-        { template:"Row 1"},
-        { template:"Row 2"}
-    ]
+        {template:"Форма 4-ДБ", height: 35},
+        {template:"Сведения об основных событиях, характеризующих состояние безопасности за прошедшую неделю с ... по ... 2015 г.", height: 35},
+        {template:"Название подразделения", height: 35},
+        {template:"Основные показатели", height: 35},
+        {cols: [
+            {
+                template:"1", width: 50
+            },
+            {
+                template:"Наиболее значимая информация об обстановке в зоне ответственности ДЗ и вокруг его объектов (информация, необходимая для доклада исполнительному вице-президенту-руководителю Департамента по безопасности и ИТ ОАО АФК \"Система\" в соответсвии с п.1.13. формы 1-ДБ):"}
 
-};
+        ], height: 95},
+        {view:"resizer"},
+        {cols: [
+            {
+                template:"1.1", width: 50
+            },
+            {
+                template:" "
+            }
+        ], height: 35},
+        {view:"resizer"},
+        {cols: [
+            {
+                template:"1.2", width: 50
+            },
+            {
+                template:" ", editor:"text"
+            }
+        ], height: 35},
+        {view:"resizer"}
+
+
+    ]//-rows tab4-DB
+
+}; //-tab_4DB
+//-----------------------------------
 var  tab5_DB = {
     id:"5DBView",
     container: "MainContainer",
     view: "template",
     template: "5-ДБ"
 };
-
-
-
+//----------------------------------------
 // при изменении размеров окна - подгоним под него размеры базового слоя
 webix.event(window, "resize", function(){
     layout.adjust();
 });
 
-
+//===================== main ===========================
 webix.ready(function(){
     layout = new webix.ui({
     container:"main_block",
